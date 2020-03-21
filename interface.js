0038ec7027c35e2602c5884f798570e7e0c1827c
Maicon Douglas <maicon.ferreira793@gmail.com>
2020-03-21 02:54:45 -0300

    Add files via upload

---
+++ interface.js
@@ -0,0 +1,6 @@
+function changerText (){
+    document.querySelector('.item1').innerHTML ='Adicione aqui qualquer conteúdo';
+}
+function changertext (){
+    document.querySelector('.item2').innerHTML = 'Aqui também';
+}
